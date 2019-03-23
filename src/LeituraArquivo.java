cf22e2e898968d6ca5ac0970c47ef69666ef5314
carloseduardoxp <carloseduardoxpto@gmail.com>
2019-03-23 08:43:20 -0300

    versao inicial

---
+++ src/LeituraArquivo.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeituraArquivo {
+
+	public BufferedReader abrirArquivo(String arquivo) throws FileNotFoundException {
+		File file = new File(arquivo);
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		return br;
+	}
+	
+	public List<String> lerArquivo(String arquivo) throws FileNotFoundException,IOException {
+		BufferedReader br = abrirArquivo(arquivo);
+		List<String> lista = new ArrayList<>();
+		while (br.ready()) {
+			lista.add(br.readLine());
+		}
+		return lista;
+	}
+	
+	public static void main(String args[]) throws IOException,FileNotFoundException {
+		LeituraArquivo la = new LeituraArquivo();
+		String dir = System.getProperty("user.dir") + "/arquivos/";
+		List<String> lista = la.lerArquivo(dir+"Cliente_20140220.txt");
+		System.out.println(lista);
+		
+	}
+
+}
